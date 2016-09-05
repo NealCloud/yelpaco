059c28e69868b8d6f610d71eb7b1f2f26a7e90c4
NealCloud <nealcloud@gmail.com>
2016-09-05 02:00:10 -0700

    Delete mysql_connect.php

--- api/mysql_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("52.35.3.250", "tacoking", "9cMLjGhVtSzsP6K4", "tacofinder");
-
-if(mysqli_connect_errno()){
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
